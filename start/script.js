1737fb8850b0899b10bac17524794614a2d49c47
WillB01 <wb.william@outlook.com>
2018-08-22 20:48:41 +0200

    push

--- start/script.js
+++ start/script.js
@@ -4,13 +4,23 @@ let nameBuilder = function(firstName = "John", age = 55){ //default value
 
 nameBuilder();
 
-/////////////////////////////////////
-for (let i = 0; i < 45; i++) {
+for (let i = 0; i < 45; i++) { //let block scooping
     let div = document.createElement('div');
     div.onclick = function() {
-        alert("message");
+        alert(`you clicked on box# ${i}`);
     };
     document.getElementsByTagName('section')[0].appendChild(div);
-
-    
 }
+
+function createEmail(firstName, purchasePrice){ //Template Strings
+    let shipping = 5.95;
+    console.log(`
+        hi ${firstName}, 
+        Thanks for buying from us!
+        Total: $${purchasePrice}
+        Shipping: $${shipping}
+        Grand Total: $${purchasePrice + shipping}
+    `)
+}
+
+createEmail("Gina",100);
